76a2be132986984c669ac209d6d8e030507df0ea
190726 <sixstar6@dbgroup.co.kr>
2024-10-25 18:39:35 +0900

    curl test

--- src/main/java/com/spring/practice/my/app/post/adapter/PostRequest.java
+++ src/main/java/com/spring/practice/my/app/post/adapter/PostRequest.java
@@ -1,7 +1,5 @@
 package com.spring.practice.my.app.post.adapter;
 
 record PostRequest(String title,
-        String content,
-        int level,
-        boolean active) {
+        String content) {
 }
