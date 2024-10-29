bc152a8074912e139590b6a252120784e925d8e2
190726 <sixstar6@dbgroup.co.kr>
2024-10-30 07:09:35 +0900

    coffee controller register test

---
+++ src/test/java/com/spring/practice/my/app/coffee/adapter/CoffeeControllerTest.java
@@ -0,0 +1,53 @@
+package com.spring.practice.my.app.coffee.adapter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spring.practice.my.app.coffee.Coffee;
+import com.spring.practice.my.app.coffee.CoffeeType;
+import com.spring.practice.my.app.coffee.ICoffees;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CoffeeController.class)
+class CoffeeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    ICoffees iCoffees;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void registerCoffee_ShouldReturnCreatedStatusAndCoffee() throws Exception {
+        //given
+        when(iCoffees.register(any())).thenReturn(Coffee.builder().name("Americano").coffeeType(CoffeeType.ESPRESSO).build());
+        // Arrange: 요청 객체 생성
+        CoffeeRequest request = new CoffeeRequest("Americano", "001");
+        String jsonRequest = objectMapper.writeValueAsString(request);
+
+        // Act & Assert
+        mockMvc.perform(post("/coffee")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Americano"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.coffeeType").value("ESPRESSO"));
+    }
+
+}
